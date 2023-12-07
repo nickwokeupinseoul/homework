3834e3c70ce2891edbb2eccc235bc06f814e44a8
Nick <johnnysniper1337@gmail.com>
2023-12-07 20:43:00 +0300

    Innit

---
+++ profile.d/maven.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/default-java
+export M2_HOME=/opt/maven
+export MAVEN_HOME=/opt/maven
+export PATH=/opt/maven/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
